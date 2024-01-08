a02fee055f5a4e9e3dcc82f7942dbb8b7e48281c
Matthias Hryniszak <padcom@gmail.com>
2024-01-08 12:11:09 +0100

    Implemented global translations

--- packages/vue/index.ts
+++ packages/vue/index.ts
@@ -4,6 +4,28 @@ import { getCurrentInstance } from 'vue-demi'
 import type { Translations } from '@padcom/vue-i18n-common'
 export type { Translations } from '@padcom/vue-i18n-common'
 
+function getAgentLocale() {
+  return globalThis.navigator?.language.split('-')[0] || 'en'
+}
+
+function getPropValue(obj: any, prop: string): any {
+  if (!obj) return null
+
+  const [current, ...rest] = prop.split('.')
+  if (rest.length === 0) {
+    if (typeof obj[current] === 'object') return undefined
+    else return obj[current]
+  } else {
+    return getPropValue(obj[current], rest.join('.'))
+  }
+}
+
+function replacePlaceholder(context: Object & Record<string, any>) {
+  return (placeholder: string, slot: string) => {
+    return context.hasOwnProperty(slot) ? context[slot] : placeholder
+  }
+}
+
 export interface CreateI18Options {
   locale?: string
   fallbackLocale?: string
@@ -14,27 +36,25 @@ export const VueI18NTranslationsSymbol = Symbol('vue-i18n-translations') as Inje
 export const VueI18NFallbackLocaleSymbol = Symbol('vue-i18n-fallback-locale') as InjectionKey<Ref<string>>
 export const VueI18NLocaleSymbol = Symbol('vue-i18n-locale') as InjectionKey<Ref<string>>
 
-function getAgentLocale() {
-  return globalThis.navigator?.language.split('-')[0] || 'en'
-}
-
 /**
- * This function unifies the how the injected values are provided
+ * vue-i18n context creator
  *
- * In the case of an application the method is `app.provide` and
- * in the case of a separate context it is just the `provide` function
- * from `'vue'` package.
+ * Enables providing the translation context in case you want
+ * to have another context in part of your system or if you
+ * are wrapping your component tree in a common web component.
+ *
+ * Counterpart to the i18n plugin
  */
-function provideContextValues(provide: any, {
+export function createI18Context({
   locale = getAgentLocale(),
   fallbackLocale = getAgentLocale(),
   messages = {},
 }: CreateI18Options) {
-  const fallbackLocaleRef = ref(fallbackLocale)
-  provide(VueI18NFallbackLocaleSymbol, ref(fallbackLocaleRef))
-
   const localeRef = ref(locale)
   provide(VueI18NLocaleSymbol, ref(localeRef))
+
+  const fallbackLocaleRef = ref(fallbackLocale)
+  provide(VueI18NFallbackLocaleSymbol, ref(fallbackLocaleRef))
 
   provide(VueI18NTranslationsSymbol, messages)
 
@@ -45,29 +65,58 @@ function provideContextValues(provide: any, {
 }
 
 /**
- * vue-i18n context creator
+ * Global i18n options
  *
- * Enables providing the translation context in case you want
- * to have another context in part of your system or if you
- * are wrapping your component tree in a common web component.
- *
- * Counterpart to the i18n plugin
+ * Those are being stored with the plugin instance.
  */
-export function createI18Context(options: CreateI18Options) {
-  return provideContextValues(provide, options)
+interface GlobalI18n {
+  locale?: Ref<string>
+  fallbackLocale?: Ref<string>
+  messages?: Translations
 }
 
 /**
- * vue-i18n plugin.
+ * Global vue-i18n instance.
  *
  * Enables providing of options for the entire application
  */
-export const i18n: Plugin = {
-  install(app: any, options: CreateI18Options) {
-    // @ts-ignore
-    window.app = app
-    if (app.provide) {
-      provideContextValues(app.provide.bind(app), options)
+const i18n: GlobalI18n = {}
+
+/**
+ * Initialize the global i18n instance
+ */
+export function createI18n({
+  locale = getAgentLocale(),
+  fallbackLocale = getAgentLocale(),
+  messages = {},
+}: CreateI18Options = {}) {
+  if (i18n.locale || i18n.fallbackLocale || i18n.messages) {
+    throw new Error('vue-i18n already initialized')
+  }
+
+  i18n.locale = ref(locale)
+  i18n.fallbackLocale = ref(fallbackLocale)
+  i18n.messages = messages
+
+  return {
+    locale: i18n.locale,
+    fallbackLocale: i18n.fallbackLocale,
+
+    t(key: string, context: Object & Record<string, any> = {}) {
+      const locale = i18n.locale?.value || getAgentLocale()
+      const fallbackLocale = i18n.fallbackLocale?.value || getAgentLocale()
+      const globalKeys = i18n.messages && i18n.messages[locale] || {}
+      const fallbackGlobalKeys = i18n.messages && i18n.messages[fallbackLocale] || {}
+
+      const translations = {
+        ...fallbackGlobalKeys,
+        ...globalKeys,
+      }
+
+      const message = getPropValue(translations, key) || key
+      const result = message.replace(/{(\w+)}/g, replacePlaceholder(context))
+
+      return result
     }
   }
 }
@@ -87,30 +136,20 @@ interface UseI18nOptions {
   useScope?: 'local-first' | 'local' | 'global'
 }
 
-export function getPropValue(obj: any, prop: string): any {
-  if (!obj) return null
-
-  const [current, ...rest] = prop.split('.')
-  if (rest.length === 0) {
-    if (typeof obj[current] === 'object') return undefined
-    else return obj[current]
-  } else {
-    return getPropValue(obj[current], rest.join('.'))
-  }
-}
-
-function replacePlaceholder(context: Object & Record<string, any>) {
-  return (placeholder: string, slot: string) => {
-    return context.hasOwnProperty(slot) ? context[slot] : placeholder
-  }
-}
-
 /**
  * Enables the use of translation in composables and setup function
  */
 export function useI18n({
   useScope = 'local-first'
 }: UseI18nOptions = {}) {
+  const locale = i18n.locale || inject(VueI18NLocaleSymbol)
+  const fallbackLocale = i18n.fallbackLocale || inject(VueI18NFallbackLocaleSymbol)
+  const global = i18n.messages || inject(VueI18NTranslationsSymbol)
+
+  if (!(locale && fallbackLocale && global)) {
+    throw new Error('vue-i18n not initialized. Either call createI18n() or createI18nContext()')
+  }
+
   const instance = getCurrentInstance()
   const local: Translations =
     // Vue 3
@@ -119,12 +158,14 @@ export function useI18n({
     (instance as any)?.proxy?.$options.i18n ||
     // Fallback
     {}
-  const global: Translations = inject(VueI18NTranslationsSymbol) || {}
-  const locale = inject(VueI18NLocaleSymbol) || ref(getAgentLocale())
-  const fallbackLocale = inject(VueI18NFallbackLocaleSymbol) || ref(getAgentLocale())
 
-  // Force re-pain of the component used here so that the language gets re-calculated
-  watch(locale, () => { instance?.update && instance?.update() })
+  // Force re-paint of the component used here so that the language gets re-calculated
+  watch(locale, () => {
+    console.log('New locale:', locale.value)
+    console.log('instance:', instance)
+    instance?.update && instance?.update()
+    instance?.proxy?.$forceUpdate && instance?.proxy?.$forceUpdate()
+  })
   watch(fallbackLocale, () => { instance?.update && instance?.update() })
 
   return {
